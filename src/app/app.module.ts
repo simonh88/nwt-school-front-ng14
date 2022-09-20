cc5af6db06a183747d17188e0f3e99c480ff4699
Simon HAJEK <s.hajek@tadaweb.com>
2022-09-20 16:49:03 +0200

    feat(step-01):

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }

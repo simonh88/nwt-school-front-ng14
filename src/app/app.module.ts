82ce2b97703b2edcc656f11fe31e4a0126772615
Simon HAJEK <s.hajek@tadaweb.com>
2023-09-12 21:41:54 +0200

    fix(): update material 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { AppComponent } from './app.component';
 
 @NgModule({
